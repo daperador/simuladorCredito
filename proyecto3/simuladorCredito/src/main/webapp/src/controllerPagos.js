05baf61b1aa71c5517a47664d8398218d1117ffa
Daniel Aperador <danielaperador@gmail.com>
2014-10-26 10:25:45 -0500

    ajuste idadministrador

--- proyecto3/simuladorCredito/src/main/webapp/src/controllerPagos.js
+++ proyecto3/simuladorCredito/src/main/webapp/src/controllerPagos.js
@@ -3,11 +3,17 @@
 /* Controllers */
 var module = angular.module('simuladorCredito.controllers', []);
 
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
 module.controller('planPagosCtrl', ['$scope', '$http', function ($scope, $http) {
         $scope.generando=false;
         $scope.datosFormulario={};
-        
-        $http.get('webresources/linea/administrador/1', {})
+        $http.get('webresources/linea/administrador/'+getParameterByName('id'), {})
         .success(function (data, status, headers, config) {
             $scope.lineas=data;
         }).error(function (data, status, headers, config) {
