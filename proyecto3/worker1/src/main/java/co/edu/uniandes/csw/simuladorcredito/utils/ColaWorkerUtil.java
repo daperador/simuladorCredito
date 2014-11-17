e14213771a20c960d4c6c155bc49e46fd514cd02
fwilchel <fwilchel@hotmail.com>
2014-11-17 16:55:34 -0500

    Quitando elementos AWS

--- proyecto3/worker1/src/main/java/co/edu/uniandes/csw/simuladorcredito/utils/ColaWorkerUtil.java
+++ proyecto3/worker1/src/main/java/co/edu/uniandes/csw/simuladorcredito/utils/ColaWorkerUtil.java
@@ -5,27 +5,18 @@
  */
 package co.edu.uniandes.csw.simuladorcredito.utils;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.PropertiesCredentials;
-import com.amazonaws.services.sqs.AmazonSQSClient;
-import com.amazonaws.services.sqs.model.CreateQueueRequest;
-import com.amazonaws.services.sqs.model.CreateQueueResult;
-import com.amazonaws.services.sqs.model.ReceiveMessageResult;
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  *
  * @author Daniel
  */
 public class ColaWorkerUtil {
-    private static AWSCredentials credentials;
+    /*private static AWSCredentials credentials;
     private static AmazonSQSClient client;
     private static CreateQueueResult response;
+    private static ReceiveMessageResult message;
+    */
     static{
-        try {
+        /*try {
             credentials=new PropertiesCredentials(new File("/tmp/dynamo.properties"));
             client = new AmazonSQSClient(credentials);
             response = client.createQueue(new CreateQueueRequest("workerQueue"));
@@ -33,28 +24,29 @@ public class ColaWorkerUtil {
             Logger.getLogger(ColaWorkerUtil.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IllegalArgumentException ex) {
             Logger.getLogger(ColaWorkerUtil.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        }*/
     }
     public static void crearMensaje(String mensaje) {
-        client.sendMessage(response.getQueueUrl(), mensaje);
+        //client.sendMessage(response.getQueueUrl(), mensaje);
     }
 
-    private static ReceiveMessageResult message;
+    
     
     public static String leerMensaje() {
-        message = client.receiveMessage(response.getQueueUrl());
+        /*message = client.receiveMessage(response.getQueueUrl());
         if(message.getMessages().isEmpty()){
             return null;
         }else{
             return message.getMessages().get(0).getBody();
-        }
+        }*/
+        return null;
     }
     
     public static void borrarUltimoMensaje(){
-        if(message!=null){
+        /*if(message!=null){
             if(!message.getMessages().isEmpty()){
                 client.deleteMessage(response.getQueueUrl(), message.getMessages().get(0).getReceiptHandle());
             }
-        }
+        }*/
     }
 }
