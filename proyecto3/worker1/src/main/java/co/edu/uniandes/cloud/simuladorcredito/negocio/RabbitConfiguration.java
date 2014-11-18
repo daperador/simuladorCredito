64d7abaa46e9bc9bb4fa321c513883cb2e5f1242
fwilchel <fwilchel@hotmail.com>
2014-11-18 00:34:22 -0500

    Cambio de la implementación de cliente de la cola

---
+++ proyecto3/worker1/src/main/java/co/edu/uniandes/cloud/simuladorcredito/negocio/RabbitConfiguration.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.uniandes.cloud.simuladorcredito.negocio;
+
+/**
+ *
+ * @author Fredy
+ */
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static java.lang.System.getenv;
+
+@Configuration
+public class RabbitConfiguration {
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        final URI ampqUrl;
+        try {
+            ampqUrl = new URI(getEnvOrThrow("CLOUDAMQP_URL"));
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        final CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setUsername(ampqUrl.getUserInfo().split(":")[0]);
+        factory.setPassword(ampqUrl.getUserInfo().split(":")[1]);
+        factory.setHost(ampqUrl.getHost());
+        factory.setPort(ampqUrl.getPort());
+        factory.setVirtualHost(ampqUrl.getPath().substring(1));
+
+        return factory;
+    }
+
+    @Bean
+    public AmqpAdmin amqpAdmin() {
+        return new RabbitAdmin(connectionFactory());
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        return new RabbitTemplate(connectionFactory());
+    }
+
+    @Bean
+    public Queue queue() {
+        return new Queue("testqueue");
+    }
+
+    private static String getEnvOrThrow(String name) {
+        final String env = getenv(name);
+        if (env == null) {
+            throw new IllegalStateException("Environment variable [" + name + "] is not set.");
+        }
+        return env;
+    }
+
+}
